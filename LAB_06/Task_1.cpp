8b586a138e4570fd556c9489cfb5f98e185d8771
OBAID <muhammadobaid103@gmail.com>
2025-11-13 23:51:38 +0500

    Checking Bash

--- LAB_06/Task_1.cpp
+++ LAB_06/Task_1.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    cout << "Hello, world!";
-    return 0;
+class MyStack{
+    int* array;
+    int top;
+    int size;
+    public:
+        MyStack(int s){
+            top=-1;
+            array = new int (s);
+            size=s;
+        }
+        void push(int data){
+            if(top==size-1){
+                cout<<"Stack OverFlow"<<endl;
+                return;
+            }
+            array[++top]=data;
+        }
+        int pop(){
+            if(top==-1){
+                cout<<"Stack Underflow"<<endl;
+                return -1;
+            }
+            return array[top--];
+        }
+        int peek(){
+            if(top==-1){
+                cout<<"Stack Underflow"<<endl;
+                return -1;
+            }
+            return array[top];
+        }
+        void Display(){
+             for(int i=top; i >= 0; i--) {
+            cout << i << "->" << array[i] << endl;
+        }
+        }
+};
+int main(){
+    MyStack S(5);
+    S.push(1);
+    S.push(2);
+    S.push(3);
+    S.push(4);
+    S.push(5);
+    cout<<"Displaying After Pushing Data"<<endl;
+    S.Display();
+    S.pop();
+    S.pop();
+    S.peek();
+    cout<<"Displaying After Popping Data"<<endl;
+    S.Display();
 }
